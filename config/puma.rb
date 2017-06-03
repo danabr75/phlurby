3d55034af7d92001110bd9cfc2812f4c8d453e5b
Benjamin Dana <ben.dana@charity.org>
2017-06-03 01:19:43 -0400

    restart puma

--- config/puma.rb
+++ config/puma.rb
@@ -23,6 +23,8 @@ environment ENV.fetch("RAILS_ENV") { "development" }
 #
 # workers ENV.fetch("WEB_CONCURRENCY") { 2 }
 
+workers 4
+
 # Use the `preload_app!` method when specifying a `workers` number.
 # This directive tells Puma to first boot the application and load code
 # before forking the application. This takes advantage of Copy On Write
