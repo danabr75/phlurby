e96cfcdfa4e8b44962d36068c79900bccc4db31c
Benjamin Dana <ben.dana@charity.org>
2018-05-24 10:46:44 -0400

    Script updates

--- examples/bin/gosu-test.sh
+++ examples/bin/gosu-test.sh
@@ -1,3 +1,4 @@
-#!/usr/bin/env bash
-
-ruby 'gosu-test/gosu-test.rb'
+#!/usr/bin/env ruby
+SCRIPT_DIRECTORY = File.expand_path('../../', __FILE__)
+require "#{SCRIPT_DIRECTORY}/gosu-test/gosu-test.rb"
+OpenGLIntegration.new.show if __FILE__ == $0
